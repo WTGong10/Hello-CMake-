20bb71f9c4d75f757f12412038d357fde163569a
wentaogong <wentaogong006@gmail.com>
2021-11-11 11:34:44 +0800

    Initial commit

---
+++ Builld/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: E:/WORK REPORT/Hello world
+# Build directory: E:/WORK REPORT/Hello world/Builld
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test1 "E:/WORK REPORT/Hello world/Builld/HelloWorld")
+set_tests_properties(test1 PROPERTIES  _BACKTRACE_TRIPLES "E:/WORK REPORT/Hello world/CMakeLists.txt;13;add_test;E:/WORK REPORT/Hello world/CMakeLists.txt;0;")
+add_test(test2 "E:/WORK REPORT/Hello world/Builld/HelloWorld" "someparameteres")
+set_tests_properties(test2 PROPERTIES  _BACKTRACE_TRIPLES "E:/WORK REPORT/Hello world/CMakeLists.txt;14;add_test;E:/WORK REPORT/Hello world/CMakeLists.txt;0;")
---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(int argc, char *argv[]) 
+{
+	if (argc > 1)
+		return 1;
+	std::cout << "hello Mr.Gong" << std::endl;
+	return 0;
+
+}
